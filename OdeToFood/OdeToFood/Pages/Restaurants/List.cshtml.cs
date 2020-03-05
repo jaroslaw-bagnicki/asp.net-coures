f62b47e97e72e58ec0cf0582a2cd24effbf13af5
Jarosław Bagnicki <jarek@bagnicki.net>
2020-03-05 13:36:43 +0100

    Add restaurant list to razor page

--- OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,23 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood
 {
     public class ListModel : PageModel
     {
         private readonly IConfiguration _config;
+        private readonly IRestaurantData _restaurantData;
 
         public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
 
-        public ListModel(IConfiguration config)
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             _config = config;
+            _restaurantData = restaurantData;
         }
 
         public void OnGet()
         {
             Message = _config["Message"];
+            Restaurants = _restaurantData.GetAll();
         }
     }
 }
