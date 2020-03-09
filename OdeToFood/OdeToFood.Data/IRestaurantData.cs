92029c6c713889f256454098daf8dc2817f220cd
Jarosław Bagnicki <jbagnicki@future-processing.com>
2020-03-09 12:44:49 +0100

    Add RestaurantData sql implementation

--- OdeToFood/OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 using System.Net.Sockets;
 using OdeToFood.Core;
 
@@ -16,68 +14,5 @@ namespace OdeToFood.Data
         Restaurant Add(Restaurant restaurant);
         Restaurant Delete(int id);
         int Commit();
-    }
-
-    public class InMemoryRestaurantData : IRestaurantData
-    {
-        List<Restaurant> restaurants;
-
-        public InMemoryRestaurantData()
-        {
-            restaurants = new List<Restaurant>
-            {
-                new Restaurant {Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian },
-                new Restaurant {Id = 2, Name = "Cinnamon Club", Location = "London", Cuisine = CuisineType.Indian },
-                new Restaurant {Id = 3, Name = "La Costa", Location = "California", Cuisine = CuisineType.Mexican },
-            };
-        }
-
-        public IEnumerable<Restaurant> GetAll()
-        {
-            return from r in restaurants
-                orderby r.Name
-                select r;
-        }
-
-        public IEnumerable<Restaurant> FilterByName(string term = "")
-        {
-            var normalizeTerm = term.ToLowerInvariant();
-            return from r in restaurants
-                where r.Name.ToLowerInvariant().Contains(normalizeTerm)
-                orderby r.Name 
-                select r;
-        }
-
-        public Restaurant GetById(int? id)
-        {
-            return restaurants.FirstOrDefault(r => r.Id == id);
-        }
-
-        public Restaurant Update(Restaurant changedRestaurant)
-        {
-            var restaurant = restaurants.FirstOrDefault(r => r.Id == changedRestaurant.Id);
-            if (restaurant == null)
-            {
-                throw(new Exception("Ups..."));
-            }
-
-            restaurant.Name = changedRestaurant.Name;
-            restaurant.Location = changedRestaurant.Location;
-            restaurant.Cuisine = changedRestaurant.Cuisine;
-
-            return restaurant;
-        }
-
-        public Restaurant Add(Restaurant newRestaurant)
-        {
-            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
-            restaurants.Add(newRestaurant);
-            return newRestaurant;
-        }
-
-        public int Commit()
-        {
-            return 0;
-        }
     }
 }
---
+++ OdeToFood/OdeToFood.Data/SqlRestaurantData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext _db;
+
+        public SqlRestaurantData(OdeToFoodDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _db.Restaurants.OrderBy(r => r.Name);
+        }
+
+        public IEnumerable<Restaurant> FilterByName(string term)
+        {
+            var normalizeTerm = term.ToLowerInvariant();
+            return _db.Restaurants
+                    .Where(r => r.Name.ToLowerInvariant().Contains(normalizeTerm))
+                    .OrderBy(r => r.Name);
+        }
+
+        public Restaurant GetById(int? id)
+        {
+            return _db.Restaurants.Find(id);
+        }
+
+        public Restaurant Update(Restaurant restaurant)
+        {
+            var entity = _db.Restaurants.Attach(restaurant);
+            entity.State = EntityState.Modified;
+            return restaurant;
+        }
+
+        public Restaurant Add(Restaurant restaurant)
+        {
+            _db.Add(restaurant);
+            return restaurant;
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = GetById(id);
+            if (restaurant == null)
+            {
+                throw (new Exception("Ups... Restaurant not found."));
+            }
+            _db.Restaurants.Remove(restaurant); 
+            return restaurant;
+        }
+
+        public int Commit()
+        {
+            return _db.SaveChanges();
+        }
+    }
+}
